4883516b2b5402249bd6f6ec4ad8ff879f8be990
Khaled <khaled@example.com>
2026-09-06 15:08:38 +0400

    feat: lock app to landscape mode

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:fahman/app/fahman_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
   runApp(const ProviderScope(child: FahmanApp()));
 }
